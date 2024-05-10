816bb06c5532e3d084d2bd63f75b7defd8e84226
Walid BOUCHAREB <bouchareb.w@gmail.com>
2024-05-10 14:29:05 +0900

    finalize nav

--- src/app/components/nav-button.tsx
+++ src/app/components/nav-button.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { ReactNode } from "react";
+import clsx from "clsx";
 
 type NavButtonProps = Readonly<
     {children: ReactNode, href: string }
@@ -7,7 +8,7 @@ type NavButtonProps = Readonly<
 
 export default function NavButton({ children, href, className }: NavButtonProps ): React.JSX.Element {
     return (
-        <Link href={href} className={`${className} rounded-tl-lg rounded-bl-lg px-4 py-2 text-center text-white bg-secondary`}>
+        <Link href={href} className={clsx("rounded-tl-lg rounded-bl-lg px-4 py-2 text-center text-white bg-secondary hover:scale-up-center", className)}>
             {children}
         </Link>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,16 +19,20 @@ export default function RootLayout({
       <body>
         <main className="flex h-full bg-background">
           <section className="flex flex-col basis-2/12">
-            <h2 className="basis-2/12 text-center"> Hive Manager </h2>
+            <header className="basis-2/12">
+              <h2 className="text-center"> Hive Manager </h2>
+            </header>
             {/* Include shared UI here e.g. a header or sidebar */}
             <nav className="flex flex-col basis-8/12 grow gap-y-[10%] ml-10">
               <NavButton href="/calendar">CALENDAR</NavButton>
               <NavButton className="bg-tertiary" href="/events">EVENTS</NavButton>
               <NavButton className="bg-tertiary" href="/office-keys">OFFICE</NavButton>
             </nav>
-            <button> Log Out</button>
+            <footer className="flex items-center content-center	flex-auto">
+              <button className="m-auto"> Log Out</button>
+            </footer>
           </section>
-          <section className="basis-10/12 bg-white">
+          <section className="basis-10/12 bg-white p-14">
             {children}
           </section>
         </main>
