589323a6e49b372ba8cc3ebaf200daf508e370ae
Florian Bretaud <lcdopg@gmail.com>
2024-05-13 12:51:00 +0900

    update locale config

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,14 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
+import type { Locale } from "./locale-config";
 import { locales } from "./locale-config";
 
 export default getRequestConfig(async ({ locale }) => {
+  const lang = locale as Locale;
+
   // Validate that the incoming `locale` parameter is valid
-  const lang = locale as (typeof locales)[number];
   if (!locales.includes(lang)) notFound();
+
   return {
     messages: (await import(`../messages/${locale}.json`)).default,
   };
--- src/locale-config.ts
+++ src/locale-config.ts
@@ -1,1 +1,3 @@
 export const locales = ["en"] as const;
+
+export type Locale = (typeof locales)[number];
