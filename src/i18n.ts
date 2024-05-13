d15989603db8bbe3ec0e77052e8346bce80367f4
Florian Bretaud <lcdopg@gmail.com>
2024-05-13 12:05:42 +0900

    shared locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,11 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-
-// Can be imported from a shared config
-const locales = ["en"];
+import { locales } from "./locale-config";
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale)) notFound();
-
+  const lang = locale as (typeof locales)[number];
+  if (!locales.includes(lang)) notFound();
   return {
     messages: (await import(`../messages/${locale}.json`)).default,
   };
