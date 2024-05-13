bd78705d899bffc35708e57ac888c90e49fe7eb5
Florian Bretaud <lcdopg@gmail.com>
2024-05-13 09:53:46 +0900

    async server component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from "next-intl";
 import { Link } from "./Link";
 import type { PropsWithChildren } from "react";
 
-export const Navigation = ({ children }: PropsWithChildren) => {
+export const Navigation = async ({ children }: PropsWithChildren) => {
   const t = useTranslations("navigation");
   return (
     <div>
