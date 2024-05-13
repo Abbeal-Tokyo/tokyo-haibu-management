b6a40ee68f715878238e7181b8391bffa65a1794
Walid BOUCHAREB <bouchareb.w@gmail.com>
2024-05-14 01:16:12 +0900

    remove client comp for feature

--- src/app/(nav-feature)/events/page.tsx
+++ src/app/(nav-feature)/events/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 // TODO: use translation library
 export default function Event() {
   return <h1>I&apos;m EVENT PAGE</h1>;
--- src/app/(nav-feature)/office-keys/page.tsx
+++ src/app/(nav-feature)/office-keys/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 // TODO: use translation library
 export default function OfficeKeys() {
   return <h1>I&apos;m OFFICE KEYS PAGE</h1>;
