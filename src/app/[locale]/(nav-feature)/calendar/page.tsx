dd476f1ed555468741627d23ea06730b34251317
Walid BOUCHAREB <bouchareb.w@gmail.com>
2024-05-21 16:41:08 +0900

    add view button change and date change

--- src/app/[locale]/(nav-feature)/calendar/page.tsx
+++ src/app/[locale]/(nav-feature)/calendar/page.tsx
@@ -1,28 +1,63 @@
 "use client";
 import { useTranslations } from "next-intl";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarDays } from "@fortawesome/free-solid-svg-icons/faCalendarDays";
+import { useCallback, useMemo, useState } from "react";
+import dayjs from "dayjs";
 import {
   Calendar as BCalendar,
   Views,
   dayjsLocalizer,
 } from "react-big-calendar";
-import dayjs from "dayjs";
-import { useMemo } from "react";
+import type { Event, View } from "react-big-calendar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons/faCalendarDays";
+
 // import jaLocal from "dayjs/locale/ja";
 // import frLocal from "dayjs/locale/fr";
 
 // dayjs.locale(frLocal);
 const localizer = dayjsLocalizer(dayjs);
 
+// TO DO : DECOMPOSE BCALENDAR BEHAVIOR TO COMPONENT
 const Calendar = () => {
   const t = useTranslations("calendar");
-  const { views }: { views: Array<keyof typeof Views> } = useMemo(
+  type ViewArray = (typeof Views)[keyof typeof Views][];
+  const { views }: { views: ViewArray } = useMemo(
     () => ({
-      views: Object.keys(Views).map((k) => Views[k]),
+      views: [Views.MONTH, Views.WEEK, Views.DAY],
     }),
     [],
   );
+  const [date, setDate] = useState(new Date());
+  const [view, setView] = useState<View>(Views.MONTH);
+  const [eventList] = useState<Event[]>([
+    {
+      title: "Event 1",
+      start: new Date("05/01/2024"),
+      end: new Date("05/01/2024"),
+    },
+    {
+      title: "Event 2",
+      start: new Date("05/05/2024"),
+      end: new Date("05/05/2024"),
+    },
+    { title: "Event 3", start: new Date(), end: new Date() },
+  ]);
+
+  const onNavigate = useCallback(
+    (newDate: Date) => {
+      console.log("Navigate");
+      setDate(newDate);
+    },
+    [setDate],
+  );
+  const onViewButtonClicked = useCallback(
+    (newView: View) => {
+      console.log("on View : ", newView);
+      setView(newView);
+    },
+    [setView],
+  );
+
   return (
     <div>
       <header>
@@ -35,12 +70,16 @@ const Calendar = () => {
         </h1>
       </header>
       <BCalendar
+        date={date}
         localizer={localizer}
-        events={[]}
+        events={eventList}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
+        view={view}
         views={views}
+        onNavigate={onNavigate}
+        onView={onViewButtonClicked}
       />
     </div>
   );
