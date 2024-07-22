aab956443a76bdace2442efdad975d7d0c89b450
Pauline <pauline.achard@abbeal.com>
2024-07-22 17:57:33 +0900

    [TOK-28] Add create event form

--- src/components/AddEvent.tsx
+++ src/components/AddEvent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { type EventForm } from "@/app/model/event";
 import { createEvent } from "@/lib/actions/event";
 import { useRef } from "react";
 import { useFormStatus } from "react-dom";
@@ -13,20 +14,59 @@ const AddButton = () => {
   );
 };
 
-export const AddEvent = () => {
+const validateForm = (formData: FormData) => {
+  const eventForm: EventForm = {
+    title: formData.get("title")?.toString() as string,
+    eventType: formData.get("eventType")?.toString() as string,
+    startDate: new Date(formData.get("startDate")?.toString() as string),
+    endDate: new Date(formData.get("endDate")?.toString() as string),
+  };
+  createEvent(eventForm);
+};
+
+export const AddEvent = (props: { eventTypes: string[] }) => {
   const ref = useRef<HTMLFormElement>(null);
   return (
     <div className="flex flex-col gap-2 left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
       <p>Add Event</p>
       <form
-        action={async (formData) => {
-          // await createEvent(formData);
+        action={(formData) => {
+          validateForm(formData);
           ref.current?.reset();
         }}
         ref={ref}
       >
-        <div className="flex flex-row gap-2">
-          <input className="text-black" type="text" name="title" />
+        <div className="flex flex-col gap-2">
+          <div>
+            Event name:
+            <input className="text-black" type="text" name="title" required />
+          </div>
+          <div>
+            Event type:
+            <input
+              className="text-black"
+              type="text"
+              name="eventType"
+              required
+            />
+          </div>
+          <div>
+            Available event types:{" "}
+            {props.eventTypes ? props.eventTypes.join(", ") : ""}
+          </div>
+          <div>
+            Start date:
+            <input
+              className="text-black"
+              type="text"
+              name="startDate"
+              required
+            />
+          </div>
+          <div>
+            End date:
+            <input className="text-black" type="text" name="endDate" required />
+          </div>
           <AddButton />
         </div>
       </form>
