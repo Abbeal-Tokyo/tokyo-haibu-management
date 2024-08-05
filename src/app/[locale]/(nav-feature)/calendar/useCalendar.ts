13e68e8143149a3d3efd6e84beba4d03ce0eee92
Pauline <pauline.achard@abbeal.com>
2024-08-05 10:46:43 +0900

    [TOK-28] Create hook for calendar + display new event on calendar

---
+++ src/app/[locale]/(nav-feature)/calendar/useCalendar.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { type Event, type View, Views } from "react-big-calendar";
+import { getEvents } from "@/lib/actions/event";
+
+const useCalendar = () => {
+  const [date, setDate] = useState(new Date());
+  const [view, setView] = useState<View>(Views.MONTH);
+  const [eventList, setEventList] = useState<Event[]>([]);
+
+  useEffect(() => {
+    getEvents()
+      .then((events) =>
+        events.map((event) => {
+          return {
+            allDay: event.end_date == null,
+            title: event.title,
+            start: event.start_date ?? undefined,
+            end: event.end_date ?? undefined,
+          };
+        }),
+      )
+      .then((events) => setEventList(events));
+  }, []);
+
+  return {
+    date,
+    view,
+    eventList,
+    onNavigate: (newDate: Date) => setDate(newDate),
+    onViewChange: (newView: View) => setView(newView),
+  };
+};
+
+export default useCalendar;
