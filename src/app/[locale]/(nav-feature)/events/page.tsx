66c95988350531fb49b383eb10f13689997a03c6
Walid BOUCHAREB <bouchareb.w@gmail.com>
2024-08-06 17:20:34 +0900

    WIP event page

--- src/app/[locale]/(nav-feature)/events/page.tsx
+++ src/app/[locale]/(nav-feature)/events/page.tsx
@@ -1,15 +1,42 @@
-import { EventForm } from "@/components/EventForm";
-import { EventsList } from "@/components/EventsList";
+import Tabs from "@/components/Tabs";
+import { faChampagneGlasses } from "@fortawesome/free-solid-svg-icons/faChampagneGlasses";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslations } from "next-intl";
+import TabEventPlanned from "./components/TabEventPlanned";
+import TabPlaces from "./components/TabPlaces";
 
 const Event = () => {
   const t = useTranslations("events");
   return (
-    <div className="flex flex-col gap-4">
-      <h1>{t("title")}</h1>
-      <EventForm />
-      <EventsList />
-    </div>
+    <>
+      <header>
+        <h1 className="text-nowrap">
+          <FontAwesomeIcon
+            className="inline w-10 h-10 mx-4"
+            icon={faChampagneGlasses}
+          />
+          {t("title")}
+        </h1>
+      </header>
+      <section className="min-h-full mx-24 my-9">
+        <Tabs
+          tabs={[
+            {
+              id: "tab-event-planned",
+              idPanel: "tabpanel-event-planned",
+              title: t("tabPlannedTitle"),
+              content: <TabEventPlanned></TabEventPlanned>,
+            },
+            {
+              id: "tab-places",
+              idPanel: "tabpanel-places",
+              title: t("tabPlacesTitle"),
+              content: <TabPlaces></TabPlaces>,
+            },
+          ]}
+        ></Tabs>
+      </section>
+    </>
   );
 };
 
